cd110e101c36aa6e84013afe3ff253f5d1a5b0dc
julian-officemanager <office.manager@nationwidemanagers.com>
2023-09-08 18:02:44 +0100

    Commit

--- data.js
+++ data.js
@@ -1,8 +1,8 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0-shej3422",
-      "name": "SHEJ3422",
+      "id": "0-toilet",
+      "name": "Toilet",
       "levels": [
         {
           "tileSize": 256,
@@ -28,11 +28,56 @@ var APP_DATA = {
         "yaw": 0,
         "fov": 1.5707963267948966
       },
-      "linkHotspots": [],
+      "linkHotspots": [
+        {
+          "yaw": -3.0392692195717395,
+          "pitch": 1.1403646036391883,
+          "rotation": 1.5707963267948966,
+          "target": "1-hall"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-hall",
+      "name": "Hall",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "yaw": 0.04646692170519273,
+        "pitch": 0.26873763226825886,
+        "fov": 1.495674328141688
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.26921242800116296,
+          "pitch": 0.3796424032509105,
+          "rotation": 4.71238898038469,
+          "target": "0-toilet"
+        }
+      ],
       "infoHotspots": []
     }
   ],
-  "name": "Project Title",
+  "name": "Hall tour",
   "settings": {
     "mouseViewMode": "drag",
     "autorotateEnabled": false,
